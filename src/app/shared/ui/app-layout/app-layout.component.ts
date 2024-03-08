346f7f1cddc8219dad208eddb3388915418e2e1e
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-03-09 00:42:17 +0100

    add app layout

--- src/app/shared/ui/app-layout/app-layout.component.ts
+++ src/app/shared/ui/app-layout/app-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-app-layout',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, MatSidenavModule],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
 })

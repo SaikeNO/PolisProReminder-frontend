433f507c712242946c349af91623a2758159c523
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-03-10 00:08:16 +0100

    add table component to policies

--- src/app/policies/policies.component.ts
+++ src/app/policies/policies.component.ts
@@ -1,15 +1,59 @@
 import { Component, inject } from '@angular/core';
 import { PoliciesSerivce } from './data-access/policies.service';
 import { CommonModule } from '@angular/common';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
+import { Policy } from '../shared/interfaces/policy';
+import { MatChipsModule } from '@angular/material/chips';
 @Component({
   selector: 'app-policies',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatChipsModule,
+  ],
   templateUrl: './policies.component.html',
   styleUrl: './policies.component.scss',
 })
 export class PoliciesComponent {
   private policyService = inject(PoliciesSerivce);
-  public policies$ = this.policyService.getPolicies();
+  public policies$ = new Observable<Policy[]>();
+  public searchQuery$ = new BehaviorSubject<string>('');
+
+  public displayedColumns: string[] = [
+    'id',
+    'title',
+    'policyNumber',
+    'insurer',
+    'insuranceCompany',
+    'startDate',
+    'endDate',
+    'paymentDate',
+    'isPaid',
+  ];
+
+  ngOnInit(): void {
+    this.policies$ = combineLatest([
+      this.searchQuery$,
+      this.policyService.getPolicies(),
+    ]).pipe(
+      map(([searchQuery, data]) =>
+        data.filter(
+          (x) =>
+            x.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            x.policyNumber.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      )
+    );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.searchQuery$.next(filterValue);
+  }
 }

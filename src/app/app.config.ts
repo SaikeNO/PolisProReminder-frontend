ed5605219f0271653ce70e9464b4d7fcfe8eaf43
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-05-01 10:21:11 +0200

    provide polish language for locale id

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,22 @@
-import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { DATE_PIPE_DEFAULT_OPTIONS, registerLocaleData } from '@angular/common';
+import localePL from '@angular/common/locales/pl';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { routes } from './app.routes';
 import { reducers } from './shared/reducers';
 import { PoliciesEffects } from './policies/data-access/state/policies.effects';
 import { httpInterceptorProviders } from './shared/interceptors';
 import { CompaniesEffects } from './insurance-company/data-access/state/companies.effects';
 import { InsuranceTypesEffects } from './insurance-types/data-access/state/insurance-types.effects';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { InsurersEffects } from './insurers/data-access/state/insurers.effects';
+
+registerLocaleData(localePL, 'pl');
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -25,5 +29,7 @@ export const appConfig: ApplicationConfig = {
     provideStore(reducers),
     provideEffects(PoliciesEffects, InsurersEffects, CompaniesEffects, InsuranceTypesEffects),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    { provide: LOCALE_ID, useValue: 'pl' },
+    { provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: { dateFormat: 'YYYY/MM/dd' } },
   ],
 };

f5eb96336a96b9f2e76bd62bcd7a31f6925ebcd6
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-05-23 21:05:49 +0200

    fix autocomplete component

--- src/app/shared/ui/autocomplete/autocomplete.component.ts
+++ src/app/shared/ui/autocomplete/autocomplete.component.ts
@@ -28,37 +28,23 @@ import { Option } from './autocomplete.model';
   styleUrl: './autocomplete.component.scss',
 })
 export class AutocompleteComponent {
-  private refresh$ = new Subject<void>();
-  private _options!: Option[];
-  @Input({ required: true }) set options(value: Option[] | null) {
-    this._options = value ? value : [];
-    this.refresh$.next();
-  }
-
-  get options(): Option[] {
-    return this._options;
-  }
-
+  @Input({ required: true }) options!: Option[];
   @Input({ required: true }) control!: FormControl;
   @Input() label: string = '';
   @Input() errorMessage: string = '';
+
   public filteredOptions!: Observable<Option[]>;
 
   ngOnInit() {
-    this.filteredOptions = combineLatest([
-      this.refresh$,
-      this.control.valueChanges.pipe(startWith('')),
-    ]).pipe(
-      map(([_, value]) => {
-        const optionValue = typeof value === 'string' ? value : value?.value;
-        return optionValue ? this.filter(optionValue as string) : this.options?.slice();
+    this.filteredOptions = this.control.valueChanges.pipe(
+      startWith<number | string>(''),
+      map((value) => {
+        return typeof value === 'string' ? this.filter(value) : this.options.slice();
       }),
     );
   }
 
   public displayFn(id: number): string {
-    if (!id) return '';
-
     let index = this.options.findIndex((option) => option.id === id);
     return this.options[index].value;
   }

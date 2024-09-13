fc472bf65e18e207e92b60ed091cc86200ffcc12
Mateusz Lengiewicz <mateusz.lengiewicz111@gmail.com>
2024-09-13 21:32:16 +0200

    add attachments in policy details

--- src/app/policies/components/policies-details/policies-details.component.ts
+++ src/app/policies/components/policies-details/policies-details.component.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { DatePipe } from '@angular/common';
 import { ErrorInfoComponent } from '../../../shared/ui/error-info/error-info.component';
+import { AttachmentsListComponent } from '../../../shared/ui/attachments-list/attachments-list.component';
+import { AttachmentParent } from '../../../shared/ui/attachments-list/data-access/attachments-list.service';
 
 export const POLICY_DETAILS = new InjectionToken<{}>('POLICY_DETAILS');
 
@@ -19,10 +21,13 @@ export const POLICY_DETAILS = new InjectionToken<{}>('POLICY_DETAILS');
     DatePipe,
     MatChipsModule,
     ErrorInfoComponent,
+    AttachmentsListComponent,
   ],
   templateUrl: './policies-details.component.html',
   styleUrl: './policies-details.component.scss',
 })
 export class PoliciesDetailsComponent {
   constructor(@Inject(POLICY_DETAILS) public policy: Policy | undefined) {}
+
+  public attachmentParentEnum = AttachmentParent;
 }
